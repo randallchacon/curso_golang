87828ad26db99d3256e9f5ec4231f2c9b2bea81e
randallchacon <info@randallchacon.com>
2022-04-29 22:58:51 -0600

    Hola mundo

---
+++ src/main.go
@@ -0,0 +1,9 @@
+package main //nombre de la carpeta donde esta guardado
+
+import "fmt"
+
+func main() { //go me obliga a escribir bien el codigo con buenas practicas
+
+	fmt.Println("Hola mundo")
+
+}
