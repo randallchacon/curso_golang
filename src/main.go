53db2c84143dfcaf1794bc0b87b1ceaa5268f7fa
randallchacon <info@randallchacon.com>
2022-05-16 18:53:37 -0600

    primer commit c27

--- src/main.go
+++ src/main.go
@@ -1,40 +1,12 @@
-package main //nombre de la carpeta donde esta guardado
+package main
 
 import "fmt"
 
-type figuras2D interface {
-	area() float64
-}
-
-type cuadrado struct {
-	base float64
-}
-
-type rectangulo struct {
-	base   float64
-	altura float64
-}
-
-func (c cuadrado) area() float64 {
-	return c.base * c.base
-}
-
-func (r rectangulo) area() float64 {
-	return r.base * r.altura
-}
-
-func calcular(f figuras2D) {
-	fmt.Println("Area: ", f.area())
+func say(text string) {
+	fmt.Println(text)
 }
 
 func main() {
-	miCuadrado := cuadrado{base: 2}
-	miRectangulo := rectangulo{base: 2, altura: 4}
-
-	calcular(miCuadrado)
-	calcular(miRectangulo)
-
-	//Lista interfaces
-	myInterface := []interface{}{"Hola", 12, 4.90}
-	fmt.Println(myInterface...)
+	say("Hello")
+	say("World")
 }
