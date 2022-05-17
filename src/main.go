5ced0c69d06b677d35531168f5d3be9e9bab7243
randallchacon <info@randallchacon.com>
2022-05-16 19:44:46 -0600

    primer commit c29

--- src/main.go
+++ src/main.go
@@ -2,15 +2,38 @@ package main
 
 import "fmt"
 
-func say(text string, c chan<- string) { //indicar si los parametros son de entrada o de salida
-	c <- text //simbolo de ingreso de datos al canal
+func message(text string, c chan string) {
+	c <- text
 }
 
-func main() { //los channels siempre son la mejor opción
-	c := make(chan string, 1)
-	fmt.Println("Hello")
+func main() {
+	c := make(chan string, 2)
+	c <- "mensaje 1"
+	c <- "mensaje 2"
 
-	go say("Bye", c)
+	fmt.Println(len(c), cap(c)) //capacidad máxima de dos
 
-	fmt.Println(<-c) // simbolo de salida de datos del canal
+	//Range y close
+	close(c) //cerrar los canales una vez que ya no se usan
+	//c <- "Mensaje 3"
+
+	for message := range c {
+		fmt.Println(message)
+	}
+
+	//Select
+	email1 := make(chan string)
+	email2 := make(chan string)
+
+	go message("mensaje 1", email1)
+	go message("mensaje 2", email2)
+
+	for i := 0; i < 2; i++ {
+		select {
+		case em1 := <-email1:
+			fmt.Println("Email recibido de email 1", em1)
+		case em2 := <-email2:
+			fmt.Println("Email recibido de email 2", em2)
+		}
+	}
 }
