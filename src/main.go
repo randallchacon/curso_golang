e982b34588dd42e010bd585fa63d977accdb40ea
randallchacon <info@randallchacon.com>
2022-05-16 19:04:32 -0600

    finalizacion de c27

--- src/main.go
+++ src/main.go
@@ -1,12 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
-func say(text string) {
+func say(text string, wg *sync.WaitGroup) {
+	defer wg.Done()
 	fmt.Println(text)
 }
 
 func main() {
-	say("Hello")
-	say("World")
+	var wg sync.WaitGroup //son complicados de mantener con el tiempo
+
+	fmt.Println("Hello")
+	wg.Add(1)
+	go say("World", &wg) //concurrencia
+	wg.Wait()
+	//time.Sleep(time.Second * 1) //esto no es eficiente, porque es contra producente
+	//las goroutines es muy comun que se usen como funciones anonimas
+
+	go func(text string) {
+		fmt.Println(text)
+	}("Adios")
+
+	time.Sleep(time.Second * 1)
 }
