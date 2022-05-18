939643b29427c0d28929e6be19d07d5c8f9780ac
randallchacon <info@randallchacon.com>
2022-05-17 21:26:27 -0600

    final c32

--- src/main.go
+++ src/main.go
@@ -1,15 +1,12 @@
 package main
 
-import (
-	"net/http"
-
-	"github.com/labstack/echo/v4"
-)
+import "fmt"
 
 func main() {
-	e := echo.New()
+	/*e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	e.Logger.Fatal(e.Start(":1323"))
+	e.Logger.Fatal(e.Start(":1323"))*/
+	fmt.Println("Hola")
 }
